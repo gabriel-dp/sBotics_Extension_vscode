ef59bdfb2b2f1a71d1175c2cb9fc3c83f0c91589
gabrieldp23 <gabrieldepaula2004@hotmail.com>
2021-04-14 10:29:12 -0300

    t

--- extension.js
+++ extension.js
@@ -1,55 +1,52 @@
-const vscode = require('vscode');
-const axios = require('axios');
-const fsLibrary = require('fs');
+const vscode = require("vscode");
+const axios = require("axios");
+const fsLibrary = require("fs");
 
-async function apiSnippets(nameLang) {
+const apiSnippets = async (nameLang) => {
     try {
-        const apiResponse = await axios.get("https://sbotics.weduc.natalnet.br/api/snippet/" + nameLang);
+        // @ts-ignore
+        const apiResponse = await axios.get(
+            "https://sbotics.weduc.natalnet.br/api/snippet/" + nameLang
+        );
         const response = {
-            "messsage": "",
-            "states": "200",
-            "response": apiResponse.data['functions']
-        }
+            messsage: "",
+            states: "200",
+            response: apiResponse.data["functions"],
+        };
         return response;
     } catch (error) {
         //Return Error
         const response = {
-            "messsage": "Error at api request!",
-            "states": "404",
-            "response": ""
-        }
+            messsage: "Error at api request!",
+            states: "404",
+            response: "",
+        };
         return response;
     }
-}
-
+};
 
 const langCount = (all) => {
-
     var R = 0;
     var C = 0;
-
-    for (let i = 0; i < all.length; i++) {
-        var lang = all[i];
-        if (lang[0] == "r") {
-            R++;
-        } else {
-            C++;
-        }
-    }
-
-    var RC = [R, C];
-    return RC;
-}
-
+    all.map((i) => {
+        const lang = i;
+        if (lang[0] == "r") R++;
+        else C++;
+    });
+    return [R, C];
+};
 
 const saveSnippet = (nameLang, snippet) => {
-    fsLibrary.writeFile(__dirname + "/snippets/" + nameLang + ".code-snippets", snippet, (erro) => {
-        if (erro) {
-            console.log("Error " + erro);
+    fsLibrary.writeFile(
+        __dirname + "/snippets/" + nameLang + ".code-snippets",
+        snippet,
+        (erro) => {
+            if (erro) {
+                console.log("Error " + erro);
+            }
         }
-    })
-}
-
+    );
+};
 
 var typeMove = [];
 var typeRead = [];
@@ -57,30 +54,30 @@ var typeWrite = [];
 var typeOthers = [];
 
 const jsonGrammars = (word, type) => {
-
     switch (type) {
         case "Movimentação":
         case "Movement":
-            typeMove.push(word)
+            typeMove.push(word);
             break;
         case "Leitura":
         case "Input":
-            typeRead.push(word)
+            typeRead.push(word);
             break;
         case "Escrita":
         case "Output":
-            typeWrite.push(word)
+            typeWrite.push(word);
             break;
         default:
-            typeOthers.push(word)
+            typeOthers.push(word);
             break;
     }
-}
-
+};
 
 const createGrammars = (sboticsLang, movement, read, write, others) => {
-
-    var grammars = fsLibrary.readFileSync(__dirname + '/support/grammars/' + sboticsLang + '.tmLanguage.json', 'utf8');
+    var grammars = fsLibrary.readFileSync(
+        __dirname + "/support/grammars/" + sboticsLang + ".tmLanguage.json",
+        "utf8"
+    );
     grammars = JSON.parse(grammars);
 
     var movementStr = "";
@@ -89,61 +86,57 @@ const createGrammars = (sboticsLang, movement, read, write, others) => {
     var othersStr = "";
 
     //#region
-    for (let m = 0; m < movement.length; m++) {
-        movementStr += movement[m] + "|";
-    }
+    movement.map((m) => (movementStr += `${m}|`));
     movementStr = movementStr.slice(0, -1);
 
-    for (let m = 0; m < read.length; m++) {
-        readStr += read[m] + "|";
-    }
+    read.map((r) => (readStr += `${r}|`));
     readStr = readStr.slice(0, -1);
 
-    for (let m = 0; m < write.length; m++) {
-        writeStr += write[m] + "|";
-    }
+    write.map((w) => (writeStr += `${w}|`));
     writeStr = writeStr.slice(0, -1);
 
-    for (let m = 0; m < others.length; m++) {
-        othersStr += others[m] + "|";
-    }
+    others.map((o) => (othersStr += `${o}|`));
     othersStr = othersStr.slice(0, -1);
     //#endregion
 
     //edit the 4 types of commands
-    grammars["repository"]["movement"]["patterns"][0]["match"] = "\\b(" + movementStr + ")\\b"
-    grammars["repository"]["read"]["patterns"][0]["match"] = "\\b(" + readStr + ")\\b"
-    grammars["repository"]["write"]["patterns"][0]["match"] = "\\b(" + writeStr + ")\\b"
-    grammars["repository"]["others"]["patterns"][0]["match"] = "\\b(" + othersStr + ")\\b"
-
+    grammars["repository"]["movement"]["patterns"][0]["match"] =
+        "\\b(" + movementStr + ")\\b";
+    grammars["repository"]["read"]["patterns"][0]["match"] =
+        "\\b(" + readStr + ")\\b";
+    grammars["repository"]["write"]["patterns"][0]["match"] =
+        "\\b(" + writeStr + ")\\b";
+    grammars["repository"]["others"]["patterns"][0]["match"] =
+        "\\b(" + othersStr + ")\\b";
 
     grammars = JSON.stringify(grammars);
 
-    fsLibrary.writeFile(__dirname + "/support/grammars/" + sboticsLang + '.tmLanguage.json', grammars, (erro) => {
-        if (erro) {
-            console.log("Error " + erro);
+    fsLibrary.writeFile(
+        __dirname + "/support/grammars/" + sboticsLang + ".tmLanguage.json",
+        grammars,
+        (erro) => {
+            if (erro) {
+                console.log("Error " + erro);
+            }
         }
-    })
-
-}
-
+    );
+};
 
 var sboticsR = 0;
 var sboticsC = 0;
 var generateGrammar = false;
 
 const jsonToSnippet = (nameLang, json, totalLangs) => {
+    var actualLang = "";
 
-    var actualLang = ''
-
-    if (nameLang[0] == 'r') {
-        actualLang = 'sboticsR';
+    if (nameLang[0] == "r") {
+        actualLang = "sboticsR";
         sboticsR++;
         if (sboticsR == totalLangs[0]) {
             generateGrammar = true;
         }
     } else {
-        actualLang = 'sboticsC';
+        actualLang = "sboticsC";
         sboticsC++;
         if (sboticsC == totalLangs[1]) {
             generateGrammar = true;
@@ -152,24 +145,25 @@ const jsonToSnippet = (nameLang, json, totalLangs) => {
 
     var Code = "{";
 
-    for (let i = 0; i < json.length; i++) {
-        const e = json[i];
+    json.map((e) => {
+        const id = e["id"].toString();
+        const name = e["name"].toString();
+        const code = e["code"].replace("()", "");
+        const description = e["description"].toString();
+        const return_type = e["return_type"].toString();
+        const type = e["type"].toString();
+        const parameters = e["parameters"];
 
-        const id = e['id'].toString();
-        const name = e['name'].toString();
-        const code = e['code'].replace('()', '');
-        const description = e['description'].toString();
-        const return_type = e['return_type'].toString();
-        const type = e['type'].toString();
-        const parameters = e['parameters'];
+        jsonGrammars(code, type);
 
-        jsonGrammars(code, type)
+        var d = description.replace(/(\r\n|\n|\r|")/gm, "");
 
-        var d = description.replace(/(\r\n|\n|\r|")/gm, "")
+        const keysLenght = Object.keys(parameters).length;
 
         var Key = [];
         var Value = [];
-        for (var p = 0; p < Object.keys(parameters).length; p++) {
+
+        for (var p = 0; p < keysLenght; p++) {
             Object.entries(parameters[p]).forEach(([key, value]) => {
                 Key.push(key);
                 Value.push(value);
@@ -179,10 +173,9 @@ const jsonToSnippet = (nameLang, json, totalLangs) => {
         Code += `
                 "${code}": {
                     "prefix": "${code}",
-                    "body": ["${code}(`
+                    "body": ["${code}(`;
 
         for (let k = 0; k < Key.length; k++) {
-
             const key = Key[k];
             const value = Value[k];
             const calc = k + 1;
@@ -190,8 +183,6 @@ const jsonToSnippet = (nameLang, json, totalLangs) => {
             Code += "${";
             Code += `${calc}:${key}[${value}]`;
             Code += "}, ";
-
-
         }
         Code = Code.replace(/,\s*$/, "");
         if (return_type == "Void" && nameLang.charAt(0) == "c") {
@@ -200,13 +191,14 @@ const jsonToSnippet = (nameLang, json, totalLangs) => {
             Code += `)`;
         }
         Code += `",],"description":"${d}"},`;
-
-
-    }
+    });
 
     //Add default snippets from support
-    var defaultSnippets = fsLibrary.readFileSync(__dirname + '/support/default/default_' + nameLang + '.json', 'utf8')
-    defaultSnippets = defaultSnippets.substring(1)
+    var defaultSnippets = fsLibrary.readFileSync(
+        __dirname + "/support/default/default_" + nameLang + ".json",
+        "utf8"
+    );
+    defaultSnippets = defaultSnippets.substring(1);
     Code += defaultSnippets;
 
     saveSnippet(nameLang, Code);
@@ -217,16 +209,14 @@ const jsonToSnippet = (nameLang, json, totalLangs) => {
         typeRead = [];
         typeWrite = [];
         typeOthers = [];
-        generateGrammar = false
+        generateGrammar = false;
     }
 };
-
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
     //Define the languagues of snippets. Please put first the reduc and then the csharp.
     var langs = [
         "reduc_en",
@@ -235,27 +225,27 @@ function activate(context) {
         //"csharp_ptbr"
     ];
 
-    var number = langCount(langs)
-
-    for (let l = 0; l < langs.length; l++) {
-        const langPrograming = langs[l];
+    var number = langCount(langs);
+    langs.map((l) => {
+        const langPrograming = l;
         const functions = apiSnippets(langPrograming);
-        Promise.resolve(functions).then(function (value) {
-            const states = value.states;
-            const message = value.messsage;
-            const response = value.response;
+        Promise.resolve(functions).then(
+            (value) => {
+                const states = value.states;
+                const message = value.messsage;
+                const response = value.response;
 
-            if (states == "404") {
-                vscode.window.showErrorMessage(message);
-            } else {
-                jsonToSnippet(langPrograming, response, number);
+                if (states == "404") {
+                    vscode.window.showErrorMessage(message);
+                } else {
+                    jsonToSnippet(langPrograming, response, number);
+                }
+            },
+            (value) => {
+                vscode.window.showErrorMessage("Ocorreu um erro inesperado!");
             }
-
-        }, function (value) {
-            vscode.window.showErrorMessage("Ocorreu um erro inesperado!");
-        });
-    }
-
+        );
+    });
 }
 exports.activate = activate;
 
